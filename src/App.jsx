1b3d837fea03edf5259f4929a007dda5617174e5
Alec Prettyman <prettale4@gmail.com>
2023-12-14 10:04:40 -0700

    ,

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { Outlet } from 'react-router-dom';
-import NavbarExample from './Components/NavbarExample.jsx';
+
 
 export default function App() {
   return (
     <>
-      <NavbarExample />
-      <Outlet />
+
     </>
   );
 }
